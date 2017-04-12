49cda220548e8fefd1b3a47e5691db6fdc201a9d
LadyTukiko <a.degroof@yahoo.de>
2017-04-12 14:23:48 +0200

    passes MitarbeiterID

--- app/src/main/java/com/example/alex/zeit/ErfassungActivity.java
+++ app/src/main/java/com/example/alex/zeit/ErfassungActivity.java
@@ -1,7 +1,9 @@
 package com.example.alex.zeit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ErfassungActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class ErfassungActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_erfassung);
+
+        Intent intent = getIntent();
+        String mitarbeiterId = intent.getStringExtra("MITARBEITER");
+
+        TextView textView = (TextView) findViewById(R.id.various);
+        textView.setText(mitarbeiterId);
+
+
     }
 }
--- app/src/main/java/com/example/alex/zeit/MainActivity.java
+++ app/src/main/java/com/example/alex/zeit/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +15,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public  void okClicked (View view){
+       //Get MitarbeiterId and PW out of the respective fields
+        EditText editText = (EditText) findViewById(R.id.mitarbeiterId);
+        String mitarbeiterId = editText.getText().toString();
+        editText = (EditText) findViewById(R.id.pw);
+        String pw = editText.getText().toString();
+
         //ToDo check if pw valid first
         Intent intent = new Intent(this, ErfassungActivity.class);
+        intent.putExtra("MITARBEITER", mitarbeiterId);
         startActivity(intent);
     }
 }
